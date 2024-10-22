82b599ce6c77fa8eaf7d42ce871df2a96585a82e
Sangdo lee <mydaniel9@gmail.com>
2024-10-23 02:00:17 +0900

    updated:put shorter example provided in the assignment requirements

--- SpinFile.py
+++ SpinFile.py
@@ -1,4 +1,4 @@
-#A4, Sangdo Lee
+#A4, Sangdo Lee https://github.com/SSangdodo/A4-Word_spinner
 from Spinner import *
 import string
 def make_list_from_file(filename):
--- Spinner.py
+++ Spinner.py
@@ -1,4 +1,4 @@
-#A4 Sangdo Lee
+#A4 Sangdo Lee https://github.com/SSangdodo/A4-Word_spinner
 from random import *
 def make_list_from_file(filename):
     file_content = open(filename,"r")
