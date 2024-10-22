c13931cdfeb3222001eaf3e00b21a98efd64f44c
Sangdo lee <mydaniel9@gmail.com>
2024-10-23 01:30:11 +0900

    updated: wrote the description for Spinner.py

--- Spinner.py
+++ Spinner.py
@@ -4,13 +4,17 @@ def make_list_from_file(filename):
     returning_list = []
     for lines in file_content:
         returning_list.append(lines.strip("\n").strip(" "))
+        #extract space and tabs from the liens
     return returning_list
 
 def make_response_dictionary(response_list):
     keyword_and_response = {}
+
     for items in response_list:
         keyword,synonyms = items.split(":")
+        #split the keyword section and synonym section
         keyword_and_response[keyword] = synonyms.split(",")
+        #store the key and synonyms into a dictonary
     return keyword_and_response
 class Spinner():
     def __init__(self,file_words):
@@ -19,10 +23,13 @@ class Spinner():
     def convert_synonyms(self):
         returning_list = []
         for words in self.file_words_list:
-            # print(words,self.synonyms_data.get(words,1))
+
             if self.synonyms_data.get(str(words),1) != 1 and randint(1,2) == 2:
+                # if the word exit within synonym dictionary and if it meets the 50% chance of changing the words
                 returning_list.append(self.synonyms_data[str(words)][randint(0,len(self.synonyms_data[words])-1)])
+                #append to the returning list of random synonyms from the specified key
             else:
                 returning_list.append(words)
+                #no match of synonym or 50% of unchanged words
 
         return " ".join(returning_list)
